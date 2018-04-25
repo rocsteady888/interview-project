5eaf744be2243534b837bdc6a59bdd7678218dcd
rocsteady888 <rocsteady888@gmail.com>
2018-04-25 16:05:36 -0400

    corrected indentation in scripts

--- scripts.js
+++ scripts.js
@@ -105,29 +105,29 @@ $(document).ready(function () {
 
     // Perfoming an AJAX GET request to our queryURL
     $.ajax({
-        url: queryURL,
-        method: "GET"
-      })
+      url: queryURL,
+      method: "GET"
+    })
 
-      // After the data from the AJAX request comes back
-      .then(function (response) {
+    // After the data from the AJAX request comes back
+    .then(function (response) {
 
-        // Saving the image_original_url property
+      // Saving the image_original_url property
 
-        // Creating and storing an image tag
-        var newImage = $("<img>");
+      // Creating and storing an image tag
+      var newImage = $("<img>");
 
-        for (j = 0; j < response.data.length; j++) {
-          $("#images").append(
-            "<img src='" + response.data[j].images.original_still.url + "'" +
-            "alt='" + response.data[j].slug + "'" +
-            "data-still='" + response.data[j].images.original.url + "'" +
-            "data-animate='" + response.data[j].images.original_still.url + "'" +
-            "data-state='animate'" +
-            "class='animalImage'>");
-          $("#images").append("<p>" + response.data[j].rating + "</p>");
-        }
-        console.log(response);
-      });
+      for (j = 0; j < response.data.length; j++) {
+        $("#images").append(
+          "<img src='" + response.data[j].images.original_still.url + "'" +
+          "alt='" + response.data[j].slug + "'" +
+          "data-still='" + response.data[j].images.original.url + "'" +
+          "data-animate='" + response.data[j].images.original_still.url + "'" +
+          "data-state='animate'" +
+          "class='animalImage'>");
+        $("#images").append("<p>" + response.data[j].rating + "</p>");
+      }
+      console.log(response);
     });
+  });
 });
